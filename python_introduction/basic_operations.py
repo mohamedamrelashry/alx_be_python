5cbde3b35463a9f48395b3150b44fe640f1be1d0
mohamedamrelashry <amrelashry8000@gmail.com>
2025-05-25 12:45:07 +0300

    done

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+# Define the variables
+number1 = 10
+number2 = 5
+
+# Calculate and print the results
+print(f"sum of {number1} and {number2} is {number1 + number2}")
+print(f"difference of {number1} and {number2} is {number1 - number2}")
+print(f"product of {number1} and {number2} is {number1 * number2}")
