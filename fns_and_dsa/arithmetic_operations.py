e2a983cb4c71169d0bb75f4211b0049afe15ccd5
mohamedamrelashry <156803087+mohamedamrelashry@users.noreply.github.com>
2025-06-11 15:59:41 +0300

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,14 +1,15 @@
-def perform_operation(num1,num2,operation ):
-        match operation:
+def perform_operation(num1, num2, operation):
+    match operation:
         case "+":
-            result= num1+num2
+            result = num1 + num2
         case "-":
-            result = num1-num2
+            result = num1 - num2
         case "*":
             result = num1 * num2
         case "/":
-            if num2==0:
-                result = "Cannot divide by zero."
-            else: 
-             result = num1/num2
+            if num2 == 0:
+                raise ValueError("Cannot divide by zero.")
+            result = num1 / num2
+        case _:
+            raise ValueError("Invalid operation")
     return result
