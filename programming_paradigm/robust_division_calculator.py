24024652a57a68dfd1e631aae06a83d98651d0d6
mohamedamrelashry <156803087+mohamedamrelashry@users.noreply.github.com>
2025-06-16 15:39:37 +0300

    Create robust_division_calculator.py

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,8 @@
+def safe_divide(numerator, denominator):
+   try:
+       result = numerator /denominator 
+       print(f"The result of the division is {result:.2f}")
+   except TypeError: 
+       print("Error: Please enter numeric values only.")
+   except ZeroDivisionError : 
+       print("Error: Cannot divide by zero.") 
